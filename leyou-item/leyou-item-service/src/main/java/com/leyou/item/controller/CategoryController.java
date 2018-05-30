a20f6741f136039c4866574a5bd5120e18d51fed
QinPengCheng <672456416@qq.com>
2018-05-30 15:19:36 +0800

    商品的分类管理已经全部完成，准备合并

--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
@@ -22,10 +22,10 @@ public class CategoryController {
 
 
     @GetMapping("list")
-    public ResponseEntity<List<Category>> queryCategoryByPid(@RequestParam(value = "pid" ,defaultValue = "0") Long pid){
+    public ResponseEntity<List<Category>> queryCategoryByPid(@RequestParam(value = "pid", defaultValue = "0") Long pid) {
         List<Category> list = categoryService.queryCategoryByPid(pid);
         //判断集合是否为空，或者查到数据长度为0
-        if (list==null||list.size()<1){
+        if (list == null || list.size() < 1) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
         return ResponseEntity.status(HttpStatus.OK).body(list);
@@ -33,29 +33,46 @@ public class CategoryController {
 
     /**
      * 添加商品分类的方法
-     * @param name
-     * @param pid
-     * @param isParent
-     * @param sort
+     *
+     * @param category
      * @return
      */
-    @PostMapping("add")
-    public ResponseEntity<Void> addCategory(Category category){
-       int result =  categoryService.addCateGory(category);
-       if (result!=1){
-           return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-       }
+    @PostMapping
+    public ResponseEntity<Void> addCategory(Category category) {
+        category.setId(null);
+        int result = categoryService.addCateGory(category);
+        if (result != 1) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
 
     @DeleteMapping("delete")
     public ResponseEntity<Void> deleteCategory(
-            @RequestParam("id")Long id
-    ){
-        int result =  categoryService.deleteCateGory(id);
-        if (result!=1){
+            @RequestParam("id") Long id
+    ) {
+        int result = categoryService.deleteCateGory(id);
+        if (result != 1) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
+
+    /**
+     * 修改商品分类的方法
+     *
+     * @param id
+     * @param name
+     * @return
+     */
+    @PutMapping
+    public ResponseEntity<Void> updateCategory(@RequestParam("id") Long id, @RequestParam("name") String name) {
+        int result = categoryService.updateCategory(id, name);
+        if (result != 1) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+
 }
--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/CategoryService.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/CategoryService.java
@@ -4,6 +4,7 @@ import com.leyou.item.mapper.CategoryMapper;
 import com.leyou.pojo.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,6 +20,7 @@ public class CategoryService {
 
     /**
      * 查询商品分类的方法
+     *
      * @param pid
      * @return
      */
@@ -30,19 +32,88 @@ public class CategoryService {
 
     /**
      * 添加商品分类
+     *
      * @param category
      * @return
      */
+    @Transactional
     public int addCateGory(Category category) {
-        category.setSort(null);
-        int result = categoryMapper.insert(category);
+        categoryMapper.insert(category);
+        //将商品分类的父亲的isparent设为父。
+        Long id = category.getParentId();
+        //将此id的isparent设置为true
+        Category parent = new Category();
+        parent.setId(id);
+        parent.setIsParent(true);
+        int result = categoryMapper.updateByPrimaryKeySelective(parent);
         return result;
     }
 
-    public int deleteCateGory(Long id) {
+
+    /**
+     * 修改商品分类
+     *
+     * @param id
+     * @param name
+     * @return
+     */
+    public int updateCategory(Long id, String name) {
         Category category = new Category();
         category.setId(id);
-        int result = categoryMapper.delete(category);
+        category.setName(name);
+        int result = categoryMapper.updateByPrimaryKeySelective(category);
         return result;
+    }
+
+    /**
+     * 删除商品分类
+     *
+     * @param id
+     * @return
+     */
+    public int deleteCateGory(Long id) {
+
+        Category category = categoryMapper.selectByPrimaryKey(id);
+        //递归删除儿子以及后辈
+        this.deleteChild(category);
+        //删除完成之后，需要判断父亲还有没有子节点，如果没有，父亲的isparent置为0
+        //找出所有的兄弟元素
+        Category c2 = new Category();
+        c2.setParentId(category.getParentId());
+        List<Category> list = categoryMapper.select(c2);
+        //当没有兄弟节点的时候
+        if (list == null || list.size()<1) {
+            //判断自己是不是根节点
+            if (category.getParentId() != 0) {
+                //将父节点的isparent置为0
+                Category parent = new Category();
+                parent.setId(category.getParentId());
+                parent.setIsParent(false);
+                categoryMapper.updateByPrimaryKeySelective(parent);
+            }
+        }
+        return 1;
+    }
+
+    /**
+     * 递归删除子节点
+     *
+     * @param category
+     */
+    public void deleteChild(Category category) {
+        //存在子节点，删除自己以及后辈
+        if (category.getIsParent()) {
+            List<Category> list = this.queryCategoryByPid(category.getId());
+            for (Category c : list) {
+                //删除这个节点，递归删除之后的节点
+                //删除自己
+                this.categoryMapper.deleteByPrimaryKey(category);
+                //删除后辈
+                this.deleteChild(c);
+            }
+        } else {
+            //如果不是父亲，直接删除
+                this.categoryMapper.deleteByPrimaryKey(category);
+        }
     }
 }
